4875ff3c88a5ce69149dd82542ee35711d48b139
Regomoditsoe <rsmramarumo@gmail.com>
2023-09-07 07:50:36 -0700

    rectangle #3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -5,15 +5,18 @@ class Rectangle {
 		if ((w > 0) && (h > 0)) {
 			this.width = w;
 			this.height = h;
+		} else {
+			this.width = 0;
+			this.height = 0;
 		}
 	}
+
 	print () {
 		for (let a = 0; a < this.height; a++) {
 			let size = '';
-			for (let b = 0; b < this.height; b++) {
+			for (let b = 0; b < this.width; b++) {
 				size += 'X';
 			}
-		}
 		console.log(size);
 		}
 	}
