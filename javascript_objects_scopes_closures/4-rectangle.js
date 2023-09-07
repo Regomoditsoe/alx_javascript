9700d44c7960820b19352dcd55a622d4f64f4903
Regomoditsoe <rsmramarumo@gmail.com>
2023-09-07 08:16:55 -0700

    Rectangle #4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+class Rectangle {
+	constructor (w, h) {
+		if ((w > 0) && (h > 0)) {
+			this.height = h;
+			this.width = w;
+		}
+	}
+	print () {
+		for (let a = 0; a < this.height; a++) {
+			let size = '';
+			for (let b = 0; b < this.width; b++) {
+				size += 'X';
+			}
+			console.log(size);
+		}
+	}
+	rotate () {
+		const aux = this width;
+		this.width = this.height;
+		this.height = aux;
+	}
+	double () {
+		this.height *= 2;
+		this.width *= 2;
+	}
+}
+module.exports = Rectangle;
