3254678d6a7c947d985cbe07075f596107521f19
Regomoditsoe <rsmramarumo@gmail.com>
2023-09-08 13:59:21 -0700

    Square #0

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -3,8 +3,8 @@
 class Rectangle {
 	constructor(w, h) {
 		if ((w > 0) && (h > 0)) {
-			this.width = h;
-			this.height = w;
+			this.width = w;
+			this.height = h;
 		} else {
 			this.width = 0;
 			this.height = 0;
