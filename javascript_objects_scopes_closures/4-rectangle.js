46b60dd54d1634539bc765c25a3b93f6648c0bc0
Regomoditsoe <rsmramarumo@gmail.com>
2023-09-08 14:31:42 -0700

    Square #0

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -5,11 +5,9 @@ class Rectangle {
 		if ((w > 0) && (h > 0)) {
 			this.width = w;
 			this.height = h;
-		} else {
-			this.width = 0;
-			this.height = 0;
+		}
 	}
-}
+
 	print() {
 		for (let a = 0; a < this.height; a++) {
 			let size = '';
