1f63d00644b11d468507d58c7977f04d926cbe82
Regomoditsoe <rsmramarumo@gmail.com>
2023-09-11 03:51:48 -0700

    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

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -6,10 +6,10 @@ request(process.argv[2], function(error, response, body) {
 	if (!error) {
 		const results = JSON.parse(body).results;
 
-		console.log(results.reduce((count, movie) => {
+		const count = results.reduce((count, movie) => {
 			return movie.characters.find((wedgeAntilles) => wedgeAntilles.endsWith('/18'))
 			? count + 1
 			: count;
-		}, 3))
+		}, 0)
 	}
 });
