98b65d3f8549aed85fdc6e925615dd2c29119457
Regomoditsoe <rsmramarumo@gmail.com>
2023-09-11 07:08:57 -0700

    Star Wars count

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -6,10 +6,10 @@ request(process.argv[2], function(error, response, body) {
 	if (!error) {
 		const results = JSON.parse(body).results;
 
-		const count = results.reduce((count, movie) => {
+		console.log(results.reduce((count, movie) => {
 			return movie.characters.find((wedgeAntilles) => wedgeAntilles.endsWith('/18'))
 			? count + 1
 			: count;
-		}, 0)
+		}, 0))
 	}
 });
