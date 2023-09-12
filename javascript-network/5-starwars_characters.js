114cf4c96c09a4a54c900ade463302c48e0941e2
Regomoditsoe <rsmramarumo@gmail.com>
2023-09-12 13:25:09 -0700

    Who was playing in this movie

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -2,23 +2,24 @@
 
 const request = require("request");
 
-id =process.argv[2];
-url = `https://swapi-api.alx-tools.com/`;
-request.get(url + id, function(error, res, body) {
+const id = process.argv[2];
+url = `https://swapi-api.hbtn.io/api/films/`;
+request.get(url + id, function(error, response, body) {
 	if (error) {
 		console.log(error);
 	}
 	
 	const data = JSON.parse(body);
-	const dd = data.characters;
-	for (const b of dd) {
-		request.get(b, function(error, res, body1) {
+	const nameChar = data.characters;
+
+	for (const b of nameChar) {
+		request.get(b, function(error, response, bodyA) {
 		if (error) {
 		console.log(error);
 		}
-			const data1 = JSON.parse(body1);
+			const dataA = JSON.parse(bodyA);
 
-			console.log(data1.name);
+			console.log(dataA.name);
 		});
 	}
 });
