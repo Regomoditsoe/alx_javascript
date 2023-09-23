bb32dcd1f2b85cbd9efb652f793084b67ee796fe
Regomoditsoe <rsmramarumo@gmail.com>
2023-09-23 07:05:09 -0700

    Closure Scope Chain

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+
+var globalVariable(welcome) {
+	function outer() {
+		// alerts contents of the variable globalVariable
+		alert(globalVariable);
+
+		// create variable named course
+		var course = "Holberton";
+
+		function inner() {
+			alert(globalVariable + " " + course);
+			var exclamation = "!";
+
+			function inception() {
+				alert(globalVariable + " " + course + exclamation);
+			}
+			inception();
+		}
+		inner();
+	}
+	outer();
+}
